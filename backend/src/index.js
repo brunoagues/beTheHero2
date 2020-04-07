0de7f2c9505827aac8d47140839881b5f6453b0f
Agues <bagues@dxc.com>
2020-04-07 15:29:37 -0300

    finalizando o backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+//qual endereço pode acessar nossa aplicaçao.
+app.use(cors(/*ex: http://meuapp.com*/));
 //para obter  o request.body no formato jason
 app.use(express.json());
 app.use(routes);
